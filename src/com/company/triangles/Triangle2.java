7ab84330bdcdc80fbd8f70ac51da06b1ff2111ad
Davit Balabekyan <93151303+davbalabekyan@users.noreply.github.com>
2022-02-17 20:59:40 +0400

    I change Triangle2

--- src/com/company/triangles/Triangle2.java
+++ src/com/company/triangles/Triangle2.java
@@ -5,19 +5,13 @@ public class Triangle2 {
     public static void main(String[] args) {
 
         System.out.println(Triangle2.checkTriangle(5, 6, 10));
-
     }
 
-    public static boolean checkTriangle(int a,int b,int c){
+    public static boolean checkTriangle(int a, int b, int c) {
 
-        if(a > 0 && b > 0 && c > 0) {
-            if ((a + b > c) && (a + c > b) && (b + c > a)) {
-                return true;
-            } else {
-                return false;
-            }
-        }else {
-            return false;
+        if (a > 0 && b > 0 && c > 0) {
+            return (a + b > c) && (a + c > b) && (b + c > a);
         }
+        return false;
     }
 }
